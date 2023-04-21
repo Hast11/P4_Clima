629a96802daefead25bd94a0abffcb735c3fc82c
Hast11 <andres2000gil@gmail.com>
2023-04-21 18:00:07 +0200

    max_north

--- practica4.js
+++ practica4.js
@@ -73,7 +73,22 @@ exports.warmer_average_temp = (cities) => {
     })
 };
 
-exports.max_north = (cities) => {};
+exports.max_north = (cities) => {
+    let maximoNorte = cities[0].coord.lon;
+    cities.forEach(ciudad =>{
+        if(cities[ciudad].coord.lon>maximoNorte){
+            maximoNorte=cities[ciudad].coord.lon;
+        }
+    })
+    
+    let ciudadMasNorte = cities.filter((ciudad)=>{
+        return ciudad.coord.lon = maximoNorte;
+    })
+    let ciudadMasNorte2 = ciudadMasNorte.map((ciudad)=>{
+        return[ciudad.name]
+    });
+    return ciudadMasNorte2;
+};
 
 exports.max_south = (cities) => {};
 
