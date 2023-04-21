97440a28f7548b3b3ed9ec9403fb5d41dbcd215a
Hast11 <andres2000gil@gmail.com>
2023-04-21 17:31:04 +0200

    max_temp_min y comentarios

--- practica4.js
+++ practica4.js
@@ -1,12 +1,10 @@
 const fs = require('fs')
 
-// Lectura fichero de forma asíncrona
 exports.load = (file) => {
     let ficheroCiudades = JSON.parse(fs.readFileSync(file));
     return ficheroCiudades;
 };
 
-// Temperatura máxima
 exports.max_temp = (cities) => {
     let temperaturaMaxima = cities[0].main.temp;
     cities.forEach(ciudad => {
@@ -19,7 +17,6 @@ exports.max_temp = (cities) => {
     return temperaturaMaxima;
 };
 
-// Temperatura mínimo
 exports.min_temp = (cities) => {
     let temperaturaMinima = cities[0].main.temp;
     cities.forEach(ciudad => {
@@ -32,26 +29,28 @@ exports.min_temp = (cities) => {
     return temperaturaMinima;
 };
 
-// Máxima temperatura mínima
-exports.max_temp_min = (cities) => {};
+exports.max_temp_min = (cities) => {
+    let temperaturaMinima = cities[0].main.temp_min;
+    cities.forEach(ciudad => {
+        let temperaturaMinimaCiudad =  ciudad.main.temp_min;
+        if (temperaturaMinimaCiudad > temperaturaMinima){
+            temperaturaMinima=temperaturaMinimaCiudad;
+        }
+        
+    });
+    return temperaturaMinima;
+};
 
-// Mínima temperatura máxima
 exports.min_temp_max = (cities) => {};
 
-// Temperatura media
 exports.average_temp = (cities) => {};
 
-// Warmer average temp
 exports.warmer_average_temp = (cities) => {};
 
-// Ciudad más al norte
 exports.max_north = (cities) => {};
 
-// Ciudad más al sur
 exports.max_south = (cities) => {};
 
-// Cálculo centro de gravedad
 exports.gravity_center = (cities) => {};
 
-// Más cercano al centro de gravedad
 exports.closest_GC = (cities) => {};
